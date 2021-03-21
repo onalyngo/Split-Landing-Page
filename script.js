b0cf27d89f6b99340e5b861f4a4e9fe9b24fbd7d
Onalyn Go <73537518+onalyngo@users.noreply.github.com>
2021-03-22 02:11:11 +0800

    add script.js file

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const right = document.querySelector('.right')
 const container = document.querySelector('.container')
 
 left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
-left.addEventListener('mouseleave', () => container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => container.classList.remove('hover-left'))
 
-left.addEventListener('mouseenter', () => container.classList.add('hover-right'))
-left.addEventListener('mouseleave', () => container.classList.add('hover-right'))
+right.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+right.addEventListener('mouseleave', () => container.classList.remove('hover-right'))
