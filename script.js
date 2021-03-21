4673beb026da9a08c8a0a74ae0bfc41dcb27aa96
Onalyn Go <73537518+onalyngo@users.noreply.github.com>
2021-03-22 02:10:12 +0800

    update index

---
+++ script.js
@@ -0,0 +1,9 @@
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+const container = document.querySelector('.container')
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-left'))
+left.addEventListener('mouseleave', () => container.classList.add('hover-left'))
+
+left.addEventListener('mouseenter', () => container.classList.add('hover-right'))
+left.addEventListener('mouseleave', () => container.classList.add('hover-right'))
